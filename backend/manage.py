4e90a7d2a20e00895a6f3c9a29ddfaced5b5175e
Tara Thierry <tarathierry@gmail.com>
2022-01-17 22:54:28 -0500

    fix: Migration issue - not working

--- backend/manage.py
+++ backend/manage.py
@@ -1,8 +1,10 @@
-flask_script import Manager
+from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app
+from app import create_app
 from models import db
+
+app = create_app()
 
 migrate = Migrate(app,db)
 manager = Manager(app)
