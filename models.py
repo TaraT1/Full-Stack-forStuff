0275972395ccd848b9f403065f8e00e76d7a0398
Tara Thierry <tarathierry@gmail.com>
2022-02-09 23:44:35 -0500

    fix: Repair migrate error DATABASE_URL => URI

--- models.py
+++ models.py
@@ -18,8 +18,7 @@ db = SQLAlchemy()
 def setup_db(app, database_path=database_path):
     #def setup_db(app):
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/stuff'
-    #app.config['SQLALCHEMY_DATABASE_URI'] = database_path #changed .config for stuff_test
-    app.config['SQLALCHEMY_DATABASE_URL'] = database_path #changed .config for stuff_test
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path #changed .config for stuff_test
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
